6cd08326251a17ce63336acfb85943137e34bb34
Giovanni Muniz <munizgiovanni@hotmail.com>
2025-06-22 17:39:21 -0300

    Maker Entry

--- backend/indicators/divap.py
+++ backend/indicators/divap.py
@@ -36,7 +36,7 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", message=".*telethon.*")
 
 # --- Constantes e Configurações Globais ---
-ENABLE_DIVAP_VERIFICATION = False
+ENABLE_DIVAP_VERIFICATION = True
 PREJUIZO_MAXIMO_PERCENTUAL_DO_CAPITAL_TOTAL = 4.90
 TAXA_ENTRADA = 0.02
 TAXA_SAIDA = 0.05
